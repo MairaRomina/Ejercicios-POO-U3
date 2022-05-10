bdcda764cbc066fa82f59396f93bf18653742e3a
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-05-10 16:43:31 -0300

    Update claseFacultad.py

--- Ej1/claseFacultad.py
+++ Ej1/claseFacultad.py
@@ -1,17 +1,17 @@
 from claseCarrera import Carrera
 
 class Facultad:
-    __codigo: int
-    __nom: str
+    __codigo: str
+    __nombre: str
     __direccion: str
     __localidad: str
     __telefono: str
-    __carreras = None
+    __carreras = None #composicion 
 
-    def __init__ (self, cod, nom, dir, lo, tel):
+    def __init__ (self, cod, nom, dire, lo, tel, lista):
         self.__codigo = cod
-        self.__nom = nom
-        self.__direccion = dir
+        self.__nombre = nom
+        self.__direccion = dire
         self.__localidad = lo
         self.__telefono = tel
         #atributos de la clase carrera
@@ -20,8 +20,13 @@ class Facultad:
         duracion = Carrera.getDuracion()
         titulo = Carrera.getTitulo()
         tipo = Carrera.getTipo()
-        __carreras = Carrera( codigo, nombre, duracion, titulo, tipo )
+        self.__carreras = Carrera( codigo, nombre, duracion, titulo, tipo ) #creo el objeto carrera dentro del objeto facultad
     
     def __str__ (self):
-        return ('Codigo: {} Nombre: {} Direccion: {} Localidad: {} Telefono: {}'.format( self.__codigo, self.__nom, self.__direccion, self.__localidad, self.__telefono ))
+        cadena ='{} Facultad: \n'.format( self.__codigo ) 
+        cadena += 'Nombre: {} Direccion: {} Localidad: {} Telefono: {}'.format( self.__nombre, self.__direccion, self.__localidad, self.__telefono )
+        cadena += str( self.__carreras ) 
+        return cadena 
+        
+         
     
