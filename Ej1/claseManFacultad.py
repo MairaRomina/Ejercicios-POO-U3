6325627b1201f5b62c7d5fe2cfa1bce75f7e6d7c
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-05-10 16:42:38 -0300

    Update claseManFacultad.py

--- Ej1/claseManFacultad.py
+++ Ej1/claseManFacultad.py
@@ -14,26 +14,11 @@ class Manejador:
         return s
     
     def agregar (self, unaFacultad):
-        self.__lista.append( unaFacultad )
+        self.__lista.append( unaFacultad ) #agrega un objeto facultad a la lista manejador
 
     def test (self):
-        archivo = open( 'facultad.csv' )
+        archivo = open( 'Facultades.csv')
         reader = csv.reader( archivo, delimiter = ';' )
-        bandera = True
-        for fila in reader:
-            cod = int(fila[0])
-            if bandera:
-                bandera = not bandera
-            elif len( cod ) == 1:
-                nom = fila[1]
-                dir = fila[2]
-                lo = fila[3]
-                tel = fila[4]
-                unaFacultad = Facultad( cod, nom, dir, lo, tel )
-                self.agregar( unaFacultad )
-        archivo.close()
-
-        reader = csv.reader()
         bandera = True
         for fila in reader:
             if bandera:
@@ -41,10 +26,11 @@ class Manejador:
                 bandera = not bandera
                 listaCarrera = []
             else:
-                if fila[0] == facu[0]:
-                    listaCarrera.append( fila )
+                if fila[0] == facu[0]: #corte de control por el id de facultad
+                    listaCarrera.append( fila ) #agrega una carrera a la lista de carreras que esta adentro del objeto facultad
                 else:
-                    nuevaFacultad( facu[0], facu[1], facu[2]..., listaCarrera )
-                    self.__lista.append( nuevaFacultad )
+                    unaFacultad = Facultad( facu[0], facu[1], facu[2], facu[3], facu[4], listaCarrera ) #creo la facultad con la lista de carreras, pero solo son filas sin especificar nada de clase carrera
+                    self.agregar( unaFacultad )
                     facu = fila
                     listaCarrera = []
+        archivo.close()
