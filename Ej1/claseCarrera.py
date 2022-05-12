0f6202512e21e7ee6ddcc51c085a45640eaf0185
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-05-11 22:46:44 -0300

    Update claseCarrera.py

--- Ej1/claseCarrera.py
+++ Ej1/claseCarrera.py
@@ -1,22 +1,27 @@
 class Carrera:
     __codigo: int
+    __codigoC: int
     __nombre: str
     __duracion: str
     __titulo: str
     __tipo: str
 
-    def __init__ (self, cod, nom, dur, tit, tipo):
+    def __init__ (self, cod, codC, nom, dur, tit, tipo):
         self.__codigo = cod
+        self.__codigoC = codC
         self.__nombre = nom
         self.__duracion = dur
         self.__titulo = tit
         self.__tipo = tipo
 
     def __str__ (self):
-        return ('Codigo: {} Nombre: {} Duracion: {} Titulo: {} Tipo: {}'.format( self.__codigo, self.__nombre, self.__duracion, self.__titulo, self.__tipo ))
+        return ('{:5} {:10} {:10} {:20} {} \n'.format( self.__codigo, self.__nombre, self.__duracion, self.__titulo, self.__tipo ))
 
     def getCodigo (self):
         return self.__codigo
+    
+    def getCodigoCarrera (self):
+        return self.__codigoC
 
     def getNombre (self):
         return self.__nombre
