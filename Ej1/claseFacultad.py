9d4f878b85585b625d7b045dbba299a794adf5ba
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-05-11 22:46:26 -0300

    Update claseFacultad.py

--- Ej1/claseFacultad.py
+++ Ej1/claseFacultad.py
@@ -6,27 +6,41 @@ class Facultad:
     __direccion: str
     __localidad: str
     __telefono: str
-    __carreras = None #composicion 
+    __carreras = [] #composicion 
 
-    def __init__ (self, cod, nom, dire, lo, tel, lista):
+    def __init__ (self, cod, nom, dire, lo, tel):
         self.__codigo = cod
         self.__nombre = nom
         self.__direccion = dire
         self.__localidad = lo
         self.__telefono = tel
         #atributos de la clase carrera
-        codigo = Carrera.getCodigo()
-        nombre = Carrera.getNombre()
-        duracion = Carrera.getDuracion()
-        titulo = Carrera.getTitulo()
-        tipo = Carrera.getTipo()
-        self.__carreras = Carrera( codigo, nombre, duracion, titulo, tipo ) #creo el objeto carrera dentro del objeto facultad
+        self.__carreras = []
     
-    def __str__ (self):
-        cadena ='{} Facultad: \n'.format( self.__codigo ) 
-        cadena += 'Nombre: {} Direccion: {} Localidad: {} Telefono: {}'.format( self.__nombre, self.__direccion, self.__localidad, self.__telefono )
-        cadena += str( self.__carreras ) 
-        return cadena 
+    def __str__(self):
+        cadena = '\n{:3} {:5} {:50} {:40} {:20} \n'.format( self.__codigo, self.__nombre, self.__direccion, self.__localidad, self.__telefono )
+        for carrera in self.__carreras:
+            cadena += str( carrera )
+        return cadena
+    
+    # def agregarCarrera( self, cod, nom, dur, titu, tipo):
+    #     unaCarrera = Carrera( cod, nom, dur, titu, tipo )
+    #     self.__carreras.append( unaCarrera )
+    
+    def agregarCarrera( self, unaCarrera ):
+        self.__carreras.append( unaCarrera )
         
+    def getIdF (self):
+        return self.__codigo
+    
+    def getNombreF (self):
+        return self.__nombre
+    
+    def getLista (self):
+        return self.__carreras
+    
+    def getLocalidad (self):
+        return self.__localidad
          
     
+
