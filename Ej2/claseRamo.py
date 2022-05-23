527ad2eec9450e8831c4da5689c7b9417e3e621b
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-05-23 19:50:24 -0300

    Update claseRamo.py

--- Ej2/claseRamo.py
+++ Ej2/claseRamo.py
@@ -1,10 +1,25 @@
 class Ramo:
     __tamaño: int
+    __lista = []
 
     def __init__(self, tam):
         self.__tamaño = tam
+        self.__lista = []
     
     def __str__ (self):
-        return 'Tamaño: {}',format( self.__tamaño )
+        return 
 
+    def __str__ (self):
+        s = '* Tamaño: {} \n'.format( self.__tamaño )
+        for ramo in self.__lista:
+            s += str( ramo ) +  '\n'
+        return s
+
+    def agregaFlor (self, unaFlor):
+        self.__lista.append( unaFlor )
+
+    def getListaRamo (self):
+        return self.__lista 
+
+    
     
