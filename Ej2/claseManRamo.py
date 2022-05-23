b449e5f52bbf364c6920d4f81d18cf1359505d47
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-05-23 19:50:00 -0300

    Update claseManRamo.py

--- Ej2/claseManRamo.py
+++ Ej2/claseManRamo.py
@@ -12,7 +12,29 @@ class ManejadorR:
             s += str( ramo ) + '\n'
         return s
     
-    def registraVenta (self, arre):
-        print(str(arre))
-        flor = input('Ingrese el numero de flor que desee: ')
-        print(flor)
+    def registraVenta (self, cant, manFlor):
+        if cant == 1:
+            unRamo = Ramo('Chico')
+        elif cant == 4:
+            unRamo = Ramo('Mediano')
+        elif cant == 6: 
+            unRamo = Ramo('Grande')
+        
+        for i in range( manFlor.getCantidad() ):
+            objeto = manFlor.getObjeto(i)
+            print(objeto.getId(), '*', objeto.getNombre(), '*', objeto.getColor())
+
+        while cant > 0:
+            num = int(input('Ingrese el numero de flor: '))
+            """ flor = manFlor.getObjeto( num - 1 ) """
+            flor = manFlor.buscaFlor( num )
+            unRamo.agregaFlor( flor ) 
+            cant -= 1
+        self.__lista.append( unRamo )
+    
+    def listar (self, manFlor):
+        lista = [ 0 for i in range( 10 )]
+        for i in range( len( self.__lista ) ):
+            listaRamo = self.__lista[i].getListaRamo()
+        
+
