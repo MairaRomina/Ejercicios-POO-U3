d7986e00b17781fe1ff4790c49d8e25cd994e96e
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-05-26 17:02:02 -0300

    Update claseManRamo.py

--- Ej2/claseManRamo.py
+++ Ej2/claseManRamo.py
@@ -14,11 +14,11 @@ class ManejadorR:
     
     def registraVenta (self, cant, manFlor):
         if cant == 1:
-            unRamo = Ramo('Chico')
+            unRamo = Ramo('chico')
         elif cant == 4:
-            unRamo = Ramo('Mediano')
+            unRamo = Ramo('mediano')
         elif cant == 6: 
-            unRamo = Ramo('Grande')
+            unRamo = Ramo('grande')
         
         for i in range( manFlor.getCantidad() ):
             objeto = manFlor.getObjeto(i)
@@ -32,9 +32,32 @@ class ManejadorR:
             cant -= 1
         self.__lista.append( unRamo )
     
-    def listar (self, manFlor):
+    """ def listar (self, manFlor):
         lista = [ 0 for i in range( 10 )]
         for i in range( len( self.__lista ) ):
-            listaRamo = self.__lista[i].getListaRamo()
+            listaRamo = self.__lista[i].getListaRamo() """
+
+    def masPedidas(self, mf):
+        listaR = [ 0 for i in range( 10 )]
+        for ramo in self.__lista:
+            listaFlores = ramo.getListaRamo()
+            for flor in listaFlores:
+                n = int(flor.getNumero())
+                listaR[n - 1] += 1
+        i = 0
+        while max(listaR) != 0 and i < 5:
+            m = listaR.index(max(listaR))
+            listaR[m] = 0
+            flor = mf.buscaFlor(m + 1)
+            print(flor.getNombre())
+            i += 1
+
+    def masVendidas(self, t):
+        for ramo in self.__lista:
+            if ramo.getTamaño() == t:
+                listaFlores = ramo.getListaRamo()
+                for flor in listaFlores:
+                    nombre = flor.getNombre()
+                    print(nombre)
         
 
