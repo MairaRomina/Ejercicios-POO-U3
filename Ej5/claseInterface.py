d2517121511c6c59b3aab89b21f76e270186f892
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-06-02 11:26:49 -0300

    Add files via upload

---
+++ Ej5/claseInterface.py
@@ -0,0 +1,10 @@
+class Interface ( Interface ):
+
+    def insertarElemento (elemento, pos):
+        pass
+
+    def agregaElemento (elemento):
+        pass
+
+    def mostrarElemento (pos):
+        pass
